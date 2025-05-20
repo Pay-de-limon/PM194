b17c6209a58c9caaaf72c3fafffa56ea57d359d8
perla moreno <122044119@upq.edu.mx>
2025-05-20 17:31:56 -0600

    practica 2 terminada

--- practica2/ejercicio_b.js
+++ practica2/ejercicio_b.js
@@ -2,7 +2,7 @@ const productos = [
     {nombre: "Laptop", precio: 12000},
     {nombre: "Mouse", precio: 250},
     {nombre: "Teclado", precio: 750},
-    {nombre: "Monitor", precio: 3000}
+    {nombre: "Monitor", precio: 3000}   
 ];
 
 const nombres = productos 
