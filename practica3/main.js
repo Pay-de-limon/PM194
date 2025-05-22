78ce3ceafbaf6138251cefad786c2f1ac0325e91
perla moreno <122044119@upq.edu.mx>
2025-05-22 12:37:07 -0600

    ejercicio_a terminado

---
+++ practica3/main.js
@@ -0,0 +1,8 @@
+
+const restar = require('./utils');
+
+
+console.log("5 - 3 =", restar(5, 3));     
+console.log("10 - 7 =", restar(10, 7));  
+console.log("20 - 30 =", restar(20, 30)); 
+console.log("0 - 0 =", restar(0, 0)); 
