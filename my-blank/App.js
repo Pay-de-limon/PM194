b2febb8bff0be37560b430f2f5df27af4bca3aef
perla moreno <122044119@upq.edu.mx>
2025-06-10 17:18:36 -0600

    avance de practica

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,33 @@
+/* zona 1: Importaciones */
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto=(props)=>{
+  const {contenido}=props
+  return(
+    <Text>{contenido}</Text>
+  )
+
+
+  };
+/* zona 2: main(ejecución) */
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Button title ="Trabaja!!"></Button>
+      <Texto contenido ="hola"></Texto>
+      <Texto contenido ="mundo"></Texto>
+      <Texto contenido ="React Native"></Texto>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* zona 3: Estética del screen */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
