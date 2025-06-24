13e7e767bc6c3ef102b343c194bd91edf25d3453
perla moreno <122044119@upq.edu.mx>
2025-06-24 16:20:57 -0600

    SplashCreen

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,70 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, Alert, ScrollView } from 'react-native';
-import React, { useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React,{useEffect, useState}from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
-export default function App() {
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [comments, setComments] = useState('');
-  const [age, setAge] = useState('');
+SplashScreen.preventAutoHideAsync();
 
-  const showAlert = () => {
-    if (name.trim() === '' || age.trim() === '') {
-      window.alert('Atención', 'Por favor, completa todos los campos obligatorios.');
-    } else {
-      window.alert(`¡Bienvenido!, Nombre: ${name}\nEdad: ${age}\nComentarios: ${comments || 'Ninguno'}`);
-    }
-  };
-
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Nombre (normal):</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu nombre"
-        value={name}
-        onChangeText={setName}
-      />
-
-      <Text style={styles.title}>Contraseña (Oculta):</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu contraseña"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry={true}
-      />
-
-      <Text style={styles.title}>Edad (numérico):</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Escribe tu edad"
-        value={age}
-        onChangeText={setAge}
-        keyboardType="numeric"
-      />
-
-      <Text style={styles.title}>Comentarios (multilínea):</Text>
-      <TextInput
-        style={[styles.input, { height: 100, textAlignVertical: 'top' }]}
-        placeholder="Escribe tus comentarios"
-        value={comments}
-        onChangeText={setComments}
-        multiline={true}
-      />
-
-      <Text style={styles.title}>Campo solo lectura:</Text>
-      <TextInput
-        style={styles.input}
-        value="Este campo no se puede editar"
-        editable={false}
-      />
-
-      <Button title="Mostrar alerta" onPress={showAlert} />
-      <StatusBar style="auto" />
-    </ScrollView>
-  );
+export default function App(){
+  const [appReady, setAppready] = useState(flase);
+  useEffect(() =>{
+    setTimeout(async () => {
+      setAppready(true);
+      await SplashScreen.hideAsync();
+    }, 2000);
+  }, []);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
